d8e5707d218e4922104612392d9b041160090693
Andres Franco <andresmfrancoc@gmail.com>
2025-03-03 11:04:38 -0600

    edit CORS

--- app/main.py
+++ app/main.py
@@ -34,7 +34,7 @@ app = FastAPI()
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Allows all origins
+    allow_origins=["http://localhost:3000"],  # Allows all origins
     allow_credentials=True,
     allow_methods=["*"],  # Allows all methods
     allow_headers=["*"],  # Allows all headers
