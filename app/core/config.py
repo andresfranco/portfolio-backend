c6d1cb707edf0978001eb8cbeaf432a4de8c916e
Andres Franco <andresmfrancoc@gmail.com>
2025-03-02 22:49:53 -0600

    change variables in config.py

--- app/core/config.py
+++ app/core/config.py
@@ -1,19 +1,21 @@
 from pydantic_settings import BaseSettings
 from pydantic import field_validator
 from typing import Any
+import os
 
 class Settings(BaseSettings):
     # Database settings
-    DATABASE_URL: str = "sqlite:///D:/Projects/PortfolioAI/portfolio-backend/test.db"
+    DATABASE_URL: str = "sqlite:///test.db"
     
     # SMTP Settings
-    SMTP_HOST: str = "mail.privateemail.com"
-    SMTP_PORT: int = 465
-    SMTP_USER: str = "admin@amfapps.com"
-    SMTP_PASSWORD: str = "test"
-    SMTP_TLS: bool = True
-    SMTP_SSL: bool = True
-
+    #Set values from .env file
+    SMTP_HOST: str = os.getenv("SMTP_HOST")
+    SMTP_PORT: int = os.getenv("SMTP_PORT")
+    SMTP_USER: str = os.getenv("SMTP_USER")
+    SMTP_PASSWORD: str = os.getenv("SMTP_PASSWORD")
+    SMTP_TLS: bool = os.getenv("SMTP_TLS")
+    SMTP_SSL: bool = os.getenv("SMTP_SSL")
+    
     SECRET_KEY: str = "your_secret_key"
     ALLOWED_HOSTS: list[str] = ["*"]
     DEBUG: bool = True
